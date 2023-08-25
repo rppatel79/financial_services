ae65c503c2115bd60cc944bf247acd26263cfea0
Ritesh Patel <ritesh.pri.patel@gmail.com>
2023-08-25 13:50:58 -0700

    Refactored to the org package

---
+++ security_master/src/main/java/org/rp/security/dao/Security.java
@@ -0,0 +1,30 @@
+package org.rp.security.dao;
+
+public class Security {
+    private String name;
+    private String symbol;
+
+    public Security(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public Security() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
